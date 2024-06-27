3454d0bb93d3dda3bc83eaa526f929045c33ce09
yamchytskyi <wk.vitalii@gmail.com>
2024-06-27 22:50:45 +0200

    Added Homers page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./App.css";
 // Lessons
 // import Lesson_02 from "./lessons/lesson_02/Lesson_02"
 
+// Homeworks
+import Homework_02 from "./homeworks/Homework_02/Homework_02";
+
 function App() {
   return (
     <div className="App">
       <p>This is my first react project</p>
       {/* <Lesson_02/> */}
+      <Homework_02 />
     </div>
   );
 }
