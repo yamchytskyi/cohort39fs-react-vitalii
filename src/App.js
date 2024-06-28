354d5d04852fb2bf8571ddf3a92748768361dfbb
yamchytskyi <wk.vitalii@gmail.com>
2024-06-28 13:39:57 +0200

    Added Consultation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import "./App.css";
 // Lessons
 // import Lesson_02 from "./lessons/lesson_02/Lesson_02"
 
+// Consultations
+// import Consultation_02 from "./consultations/Consultation_01/Consultation_01"
+
 function App() {
   return (
     <div className="App">
       <p>This is my first react project</p>
+      {/* Lessons: */}
       {/* <Lesson_02/> */}
+
+      {/* Consultations: */}
+      {/* <Consultation_02 /> */}
     </div>
   );
 }
---
+++ src/consultations/Consultation_01/data.js
@@ -0,0 +1,6 @@
+export const userData = {
+  userName: "Tom",
+  avatar:
+    "https://www.redwolf.in/image/cache/catalog/stickers/tom-face-sticker-india-600x800.jpg?m=1687858270",
+
+};
