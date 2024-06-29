9a20a7c1e4758aa21d68f242af92a74d1cbfb74c
yamchytskyi <wk.vitalii@gmail.com>
2024-06-29 16:34:25 +0200

    Added lesson_03

---
+++ src/homeworks/Homework_02/Homework_02_solution.jsx
@@ -0,0 +1,9 @@
+import SimpsonsCard from "../..//components/SimpsonsCard/SimpsonsCard"
+
+function Homework_02_solution() {
+    return (
+        <SimpsonsCard />
+    )
+}
+
+export default SimpsonsCard;
