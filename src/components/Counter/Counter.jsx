7a0a0c81ae5781fca3f24352d71d33d8bc1d232c
yamchytskyi <wk.vitalii@gmail.com>
2024-07-02 13:27:25 +0200

    Complete Lesson 4

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,42 @@
+import "./styles.css";
+import Button from "../../components/Button/Button";
+
+// Импорт функции из библиотеки React
+import { useState } from "react";
+
+function Counter() {
+  // Функция-хук useState() возвразает массив их 2х элементов
+  // - 1 элемент - это переменная состояния
+  // - 2 функция, которая меняет это состояние
+  // По умолчанию count = undefind, если мы ничего не передаем в качестве аргумента в функцию useState()
+  const [count, setCount] = useState(0);
+  console.log(count);
+  console.log(setCount);
+
+  const onPlus = () => {
+    setCount((prevValue)  => {
+        console.log(prevValue)
+        return prevValue + 1;
+    })
+  }
+
+  const onMinus = () => {
+    setCount((prevValue) => {
+        return prevValue - 1;
+    })
+  }
+
+  return (
+    <div className="counter-wrapper">
+      <div className="button-control">
+        <Button name="-" onClick={onMinus} />
+      </div>
+      <p className="count">{count}</p>
+      <div className="button-control">
+        <Button name="+" onClick={onPlus} />
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
