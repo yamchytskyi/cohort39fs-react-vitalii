00cf223cd4358aa233d38bcc1e2db4f9c86ae327
yamchytskyi <wk.vitalii@gmail.com>
2024-07-02 19:47:14 +0200

    better styles for button

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -6,29 +6,29 @@ function Feedback() {
   const [likes, setLikes] = useState(0);
   const [dislikes, setDislikes] = useState(0);
 
-  const toLike = () => {
+  const onLike  = () => {
     setLikes((prevValue) => {
       return prevValue + 1;
     });
   };
 
-  const toDislike = () => {
+  const onDislike = () => {
     setDislikes((prevValue) => {
       return prevValue + 1;
     });
   };
 
-  const toReset = () => {
+  const resetResults = () => {
     setDislikes(0);
     setLikes(0);
   };
   return (
     <div className="wrapper">
-      <Button name="Like" onClick={toLike} />
+      <Button name="Like" onClick={onLike } />
       <p className="likes-counter">{likes}</p>
-      <Button name="Dislake" onClick={toDislike} />
+      <Button name="Dislake" onClick={onDislike} />
       <p className="dislikes-counter">{dislikes}</p>
-      <Button name="Reset Results" onClick={toReset} />
+      <Button name="Reset Results" onClick={resetResults} />
     </div>
   );
 }
